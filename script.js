c360ac16194b34e3d4e01f0da00f1abf356f03c2
Jenny Yu <jenongrab@gmail.com>
2025-10-17 17:10:33 +1100

    adjust mouseenter to mouseover

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-
-const button = document.getElementsByClassName(`id`);
-
-button.addEventListener(`mouseover`, )
-
 // 1. listen for mouse hover, add glow if hover
 // 2. listen for button press, if pressed, load API 
+
+const button = document.getElementsById("button");
+
+button.addEventListener("mouseover", () => {
+    button.classList.add("button-glow");
+    
+});
+
+button.addEventListener("mouseout", () => {
+    button.classList.remove("button-glow");
+});
