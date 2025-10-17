4037b9b8cdd8d241a72e8ea78d21243bce13d5cf
Jenny Yu <jenongrab@gmail.com>
2025-10-17 15:26:11 +1100

    edit js to listen for button enter and leave

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const button = document.getElementsByClassName(`id`);
+
+button.addEventListener(`mouseover`, )
+
+// 1. listen for mouse hover, add glow if hover
+// 2. listen for button press, if pressed, load API 
