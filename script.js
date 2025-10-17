00119dc773b4945ebae7dd5a86e203df19714535
Jenny Yu <jenongrab@gmail.com>
2025-10-17 17:24:39 +1100

    debug getelementbyid

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // 1. listen for mouse hover, add glow if hover
+// a. check domloaded
 // 2. listen for button press, if pressed, load API 
 
-const button = document.getElementsById("button");
+
+const button = document.getElementById("button");
 
 button.addEventListener("mouseover", () => {
     button.classList.add("button-glow");
@@ -11,3 +13,22 @@ button.addEventListener("mouseover", () => {
 button.addEventListener("mouseout", () => {
     button.classList.remove("button-glow");
 });
+
+
+//   window.addEventListener("DOMContentLoaded", () => {
+//     const button = document.getElementById("button");
+
+//     if (button) {
+//       button.addEventListener("mouseover", () => {
+//         button.classList.add("button-glow");
+//         console.log("hovered");
+//       });
+
+//       button.addEventListener("mouseout", () => {
+//         button.classList.remove("button-glow");
+//       });
+//     } else {
+//       console.error("Button with id='button' not found");
+//     }
+//   });
+
